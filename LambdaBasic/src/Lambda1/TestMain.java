5d9ef9c0519d1dcb5c3642029c98eedb83201427
Boom <Boom@192.168.163.128>
2017-09-03 00:10:41 +0530

    TESTING - AR - 03092017

---
+++ LambdaBasic/src/Lambda1/TestMain.java
@@ -0,0 +1,38 @@
+package Lambda1;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		//ANNONYMUS INNER CLASS
+		
+		System.out.println(process("Hello World",new Processor(){
+			public String process(String s){
+				return s.toLowerCase();
+			}
+		}));
+		
+		//LAMBDE EXPRESSION
+/*
+		System.out.println(process("Hello World",(String s) -> {
+				return s.toLowerCase();
+			}
+		));*/
+		
+
+		//System.out.println(process("Hello World",(String s) ->	s.toUpperCase()));
+		
+		//System.out.println(process("Hello World",(s) ->	s.toUpperCase()));
+	}
+
+	private static String process(String s, Processor p) {
+		
+		return p.process(s);
+	}
+	
+	@FunctionalInterface
+	public interface Processor{
+		public String process(String s);
+	}
+
+}
